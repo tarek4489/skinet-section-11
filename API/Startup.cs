e1ebef15e4b233cd3dc502253ea6dadbd3fd2a5e
Tarek Ibrahim <tibrahim@ccprmg.corp>
2021-08-04 16:29:54 -0300

    End of section 6

--- API/Startup.cs
+++ API/Startup.cs
@@ -30,6 +30,12 @@ namespace API
 
             services.AddApplicationServices();
             services.AddSwaggerDocumentation();
+            services.AddCors(opt => {
+                opt.AddPolicy("CorsPolicy", policy => 
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
+                });
+            });
 
         }
 
@@ -45,6 +51,8 @@ namespace API
             app.UseRouting();
 
             app.UseStaticFiles();
+
+            app.UseCors("CorsPolicy");
 
             app.UseAuthorization();
 
